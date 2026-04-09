b2dc7a04a2c6270af8c8ae44ff43716cb01c1630
Elyá <elyavyale@gmail.com>
2026-04-08 17:20:55 -0700

    Update wolfknight_generator.js

--- js/generators/wolfknight_generator.js
+++ js/generators/wolfknight_generator.js
@@ -12,17 +12,15 @@ function PointRollD6() {
 function PointRollD6s(num) {
 	sum = 0;
 	for (let i = 0; i < num; i++) {
-		j = GetRandomInt(6)
-		if (j == 5) sum = sum + 2
-		else if (j == 3 || j == 4) sum = sum + 1
+		sum = sum + PointRollD6()
 	}
 	return sum;
 }
 
 function RollWK() {
-    document.getElementById("attribute_v").innerHTML = PointRollD6() + 1;
-    document.getElementById("attribute_i").innerHTML = PointRollD6() + 1;
-    document.getElementById("attribute_c").innerHTML = PointRollD6() + 1;
+    document.getElementById("attribute_v").innerHTML = PointRollD6s(2) + 1;
+    document.getElementById("attribute_i").innerHTML = PointRollD6s(2) + 1;
+    document.getElementById("attribute_c").innerHTML = PointRollD6s(2) + 1;
     document.getElementById("guard_e").innerHTML = PointRollD6s(3) + 4;
     document.getElementById("guard_w").innerHTML = PointRollD6s(3) + 4;
 }
